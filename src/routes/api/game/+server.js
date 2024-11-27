aed12c0afa0ee305e7489770bee15befb21ae578
JohnnyCaringi <JohnnyCaringi9@gmail.com>
2024-11-27 16:29:52 -0600

    Betting actually removes or adds joes now

--- src/routes/api/game/+server.js
+++ src/routes/api/game/+server.js
@@ -1,22 +1,21 @@
 import {connectToDB} from "$lib/db.js";
 import {json} from "@sveltejs/kit";
+import {ObjectId} from "mongodb"
 
 
 export async function POST({ request }) {
 	const data = await request.json();
-
 	let game = data.game;
 	let guess = data.guess;
 	let bet = data.bet;
-	const db = await connectToDB();
-	const users = db.collection("users");
+	let userID = data.userID;
 
 	let message = "";
 	let joes = 0;
 
 	switch(game){
 		case "coin":
-			[message, joes] = coin(bet, guess);
+			[message, joes] = await coin(bet, guess, userID);
 			break;
 		default:
 			console.log("ERROR: GAME NOT FOUND");
@@ -27,7 +26,11 @@ export async function POST({ request }) {
 	return json({ success: true, message: message, joes: joes});
 }
 
-function coin(bet, guess){
+async function coin(bet, guess, ID){
+	const db = await connectToDB();
+	const users = db.collection("users");
+	ID = new ObjectId(ID);
+
 	let result = "";
 	let netGain = 0;
 	//Generate a random number between 0 and 1
@@ -51,5 +54,6 @@ function coin(bet, guess){
 	else{
 		netGain = 0 - bet;
 	}
+	await users.updateOne({_id: ID}, { $inc: { joes: netGain } });
 	return [result, netGain];
 }
--- src/routes/coin-flip/+page.server.js
+++ src/routes/coin-flip/+page.server.js
@@ -1,13 +1,25 @@
+import { redirect } from '@sveltejs/kit'
+
+let userID = '';
+
+export async function load({ locals }) {
+    if (locals.user == '') {
+        redirect (302, "/login")
+    } else {
+        userID = locals.user;
+    }
+}
+
 /** @type {import('./$types').Actions} */
 export const actions = {
-    "coin-flip": async ({ request, fetch }) => {
+    "coin-flip": async ({ request, fetch}) => {
         const requestData = await request.formData();
         let fetchOpts = {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({game: "coin", guess: requestData.get("side"), bet: requestData.get("joes")})
+            body: JSON.stringify({game: "coin", guess: requestData.get("side"), bet: requestData.get("joes"), userID: userID})
         }
 
         const response = await fetch("/api/game", fetchOpts);
